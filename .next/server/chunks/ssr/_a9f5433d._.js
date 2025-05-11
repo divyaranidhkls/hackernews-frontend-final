eb7b697fdcb8f59341a74ea73672bbb853560430
Divyarani K <divyaranidhkls@gmail.com>
2025-05-11 09:51:24 +0530

    Add source map for SSR chunk and include Unauthorized component mapping
    - Generated source map for the SSR chunk located at .next/server/chunks/ssr/_b78aa0c2._.js.map.
    - Mapped Unauthorized component and its dependencies for better debugging.
    - Included conditional exports for app-page module based on environment variables.
    - Injected loader tree and pages for route module integration.

--- .next/server/chunks/ssr/_a9f5433d._.js
+++ .next/server/chunks/ssr/_a9f5433d._.js
@@ -227,7 +227,7 @@ function PostPage() {
     const [totalPages, setTotalPages] = (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$server$2f$route$2d$modules$2f$app$2d$page$2f$vendored$2f$ssr$2f$react$2e$js__$5b$app$2d$ssr$5d$__$28$ecmascript$29$__["useState"])(1);
     const router = (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$navigation$2e$js__$5b$app$2d$ssr$5d$__$28$ecmascript$29$__["useRouter"])();
     const limit = 5;
-    const API_BASE = ("TURBOPACK compile-time value", "http://hackernews.ambitiouscliff-17e19e82.centralindia.azurecontainerapps.io");
+    const API_BASE = ("TURBOPACK compile-time value", "https://hackernews.ambitiouscliff-17e19e82.centralindia.azurecontainerapps.io");
     const fetchPosts = (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$server$2f$route$2d$modules$2f$app$2d$page$2f$vendored$2f$ssr$2f$react$2e$js__$5b$app$2d$ssr$5d$__$28$ecmascript$29$__["useCallback"])(async (pageNum)=>{
         setLoading(true);
         setError("");
